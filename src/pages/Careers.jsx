6adc81073d60ff6c4958e8d4cba9f33414de9c2e
Sai Kiran <neelakantamsaikiran2@gmail.com>
2025-02-10 13:52:00 +0530

    Test Husky setup

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 export default function Careers() {
-
+    function greet(name) {
+        console.log("Hello" + name)
+      }
+      greet("sai")
   return (
     <div>Careers</div>
   )
